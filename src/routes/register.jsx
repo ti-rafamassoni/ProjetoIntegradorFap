25e65d4a129c49d1c6dd02f2ec5e989bbe7015f3
Rafael Massoni <ti.rafamassoni@gmail.com>
2023-12-12 12:22:07 -0300

    Implementação de validações no registro

--- src/routes/register.jsx
+++ src/routes/register.jsx
@@ -25,10 +25,53 @@ export default function Register() {
     const navigate = useNavigate()
     const users = JSON.parse(localStorage.getItem('users')) || [];
     const [showAlert, setShowAlert] = React.useState(false)
+    const [errors, setErrors] = React.useState({});
+
+    const validateForm = (formData) => {
+      const newErrors = {};
+    
+      for (const [name, value] of formData.entries()) {
+        if (!value) {
+          newErrors[name] = 'Este campo é obrigatório.';
+        }
+      }
+
+      const email = formData.get('email');
+      const birth = formData.get('birth');
+      const password = formData.get('password');
+      const confirmPassword = formData.get('confirmPassword');
+
+      const emailRegex = /^[\w.-]+@[\w.-]+\.[a-z]{2,4}$/;
+      if (email && !emailRegex.test(email)) {
+        newErrors['email'] = 'E-mail inválido.';
+      }
+
+      const dateRegex = /^\d{2}\/\d{2}\/\d{4}$/;
+      if (birth && !dateRegex.test(birth)) {
+        newErrors['birth'] = 'Data de nascimento inválida.';
+      }
+
+      if (password && password.length < 6) {
+        newErrors['password'] = 'A senha deve ter pelo menos 6 dígitos.';
+      }
+
+      if (confirmPassword && password !== confirmPassword) {
+        newErrors['confirmPassword'] = 'As senhas não são iguais.';
+      }
+
+    
+      setErrors(newErrors);
+      return Object.keys(newErrors).length === 0;
+    };
+
     const handleSubmit = (event) => {
       event.preventDefault();
 
       const formData = new FormData(event.target);
+
+  if (!validateForm(formData)) {
+    return;
+  }
 
       const name = formData.get("name")
       const email = formData.get("email")
@@ -40,23 +83,19 @@ export default function Register() {
 
       const emailRegex = /^[\w.-]+@[\w.-]+\.[a-z]{2,4}$/;
       if (!emailRegex.test(email)) {
-        alert("E-mail inválido")
       return;
       }
 
       const dateRegex = /^\d{2}\/\d{2}\/\d{4}$/;
       if (!dateRegex.test(birth)) {
-        alert('Data de nascimento inválida!');
         return;
       }
 
       if (password.length < 6) {
-        alert('A senha deve ter pelo menos 6 dígitos!');
         return;
       }
   
       if(password !== confirmPassword) {
-        alert("As senhas não são iguais");
         return;
       }
   
@@ -80,8 +119,8 @@ export default function Register() {
         setShowAlert(false);
         navigate('/usertable');
     }, 3000); // Oculta o alerta após 3 segund
-     
-  };
+  }   
+  
   return (
     <ThemeProvider theme={Theme}>
       <Box
@@ -94,13 +133,20 @@ export default function Register() {
       noValidate
       autoComplete="on"
       onSubmit={handleSubmit}>
-        <TextField id="nameInput" name='name'  label="Nome Completo" variant="standard" type='text' required />
-        <TextField id="emailInput" name='email'  label="Email" variant="standard" type='email' required />
-        <TextField id="birthInput" name='birth'  label="Data de Nascimento" variant='standard' required />
-        <TextField id="jobInput" name='job'  label="Cargo" variant="standard" type='text' required />
-        <TextField id="sectorInput" name='sector'  label="Setor" variant="standard" type='text' required />
-        <TextField id="passwordInput" name='password'  label="Senha (seis dígitos)" variant="standard" required type='password' minlenght={6} />
-        <TextField id="confirmPasswordInput" name='confirmPassword'  label="Confirme a senha" variant="standard" required type='password' />
+        <TextField id="nameInput" name='name'  label="Nome Completo" variant="standard" type='text' required error={!!errors['name']}
+          helperText={errors['name']} />
+        <TextField id="emailInput" name='email'  label="Email" variant="standard" type='email' required error={!!errors['email']}
+          helperText={errors['email']}/>
+        <TextField id="birthInput" name='birth'  label="Data de Nascimento" variant='standard' required error={!!errors['birth']}
+          helperText={errors['birth']} />
+        <TextField id="jobInput" name='job'  label="Cargo" variant="standard" type='text' required error={!!errors['job']}
+          helperText={errors['job']}/>
+        <TextField id="sectorInput" name='sector'  label="Setor" variant="standard" type='text' required error={!!errors['sector']}
+          helperText={errors['sector']}/>
+        <TextField id="passwordInput" name='password'  label="Senha (seis dígitos)" variant="standard" required type='password' minlenght={6} error={!!errors['password']}
+          helperText={errors['password']}/>
+        <TextField id="confirmPasswordInput" name='confirmPassword'  label="Confirme a senha" variant="standard" required type='password' error={!!errors['confirmPassword']}
+          helperText={errors['confirmPassword']} />
         <Button id="registerButton" variant="contained" type="submit">Cadastrar</Button>
       </Box>
       {showAlert && <RegisterAlert />}
